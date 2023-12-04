6e08aaec1ba2b165d0efe11557c4fbaec2d05500
Travis Mick <root@lo.calho.st>
2023-12-04 13:51:36 +0000

    cleanup

--- keyboards/crkbd/keymaps/tmick0/keymap.c
+++ keyboards/crkbd/keymaps/tmick0/keymap.c
@@ -43,7 +43,7 @@ M1(KC_EXLM),   M2(KC_AT), M3(KC_HASH),  M4(KC_DLR), KC_PERC,                    
       KC_F1,       KC_F2,       KC_F3,       KC_F4,   KC_F5,                                           KC_F6,       KC_F7,       KC_F8,       KC_F9,      KC_F10,
      KC_F11,      KC_F12,     XXXXXXX,     XXXXXXX, XXXXXXX,                                         XXXXXXX,     XXXXXXX,     XXXXXXX,     XXXXXXX,     XXXXXXX,
     XXXXXXX,     XXXXXXX,     XXXXXXX,     XXXXXXX, XXXXXXX,                                         XXXXXXX,     XXXXXXX,     XXXXXXX,     XXXXXXX,     XXXXXXX,
-                                           XXXXXXX, XXXXXXX, XXXXXXX,                       _______,  KC_EQL,      KC_DEL
+                                           XXXXXXX, XXXXXXX, XXXXXXX,                       _______, XXXXXXX,     XXXXXXX
 ),
 };
 
