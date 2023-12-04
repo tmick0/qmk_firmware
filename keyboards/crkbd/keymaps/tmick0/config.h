7f67626bda8d9b1d0d496ee16eaf73d879327390
Travis Mick <root@lo.calho.st>
2023-12-04 06:54:00 +0000

    Add custom crkbd keymap

---
+++ keyboards/crkbd/keymaps/tmick0/config.h
@@ -0,0 +1,8 @@
+// Copyright 2023 Travis Mick (@tmick0)
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+#define TAPPING_TERM 200
+#define LT_TAPPING_TERM 200
+#define TAPPING_TERM_PER_KEY
+#define PERMISSIVE_HOLDS
