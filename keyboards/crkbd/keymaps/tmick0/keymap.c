2add68635b0dbb693b176461aa2741e1726e0548
Travis Mick <root@lo.calho.st>
2024-03-08 18:50:00 +0000

    Update keymap for 34 keys

--- keyboards/crkbd/keymaps/tmick0/keymap.c
+++ keyboards/crkbd/keymaps/tmick0/keymap.c
@@ -22,14 +22,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
        KC_Q,       KC_W,         KC_E,        KC_R, KC_T,                                               KC_Y,        KC_U,        KC_I,        KC_O,        KC_P,
    M1(KC_A),   M2(KC_S),     M3(KC_D),    M4(KC_F), KC_G,                                               KC_H,    M4(KC_J),    M3(KC_K),    M2(KC_L), M1(KC_SCLN),
    M2(KC_Z),   M3(KC_X),     M4(KC_C),        KC_V, KC_B,                                               KC_N,        KC_M,     KC_COMM,      KC_DOT,     KC_SLSH,
-                                           KC_LBRC, KC_RBRC, LT(_NUM, KC_SPC),     LT(_NAV, KC_ENT), KC_QUOT,     KC_BSPC
+                                           XXXXXXX, KC_LBRC, LT(_NUM, KC_SPC),     LT(_NAV, KC_ENT), KC_BSPC,     XXXXXXX
 ),
 
 [_NUM] = LAYOUT_split_3x5_3(
        KC_1,        KC_2,        KC_3,        KC_4,    KC_5,                                            KC_6,        KC_7,        KC_8,        KC_9,        KC_0,
 M1(KC_EXLM),   M2(KC_AT), M3(KC_HASH),  M4(KC_DLR), KC_PERC,                                         KC_CIRC, M4(KC_AMPR), M3(KC_ASTR), M2(KC_LPRN), M1(KC_RPRN),
-    XXXXXXX,     XXXXXXX,     XXXXXXX,     XXXXXXX, XXXXXXX,                                          KC_GRV,     KC_BSLS,     KC_MINS,     XXXXXXX,     XXXXXXX,
-                                           XXXXXXX, XXXXXXX, XXXXXXX,                       MO(_FN),  KC_EQL,      KC_DEL
+      KC_F1,       KC_F2,       KC_F3,       KC_F4,   KC_F5,                                          KC_GRV,     KC_QUOT,     KC_MINS,     KC_EQL,     KC_BSLS,
+                                           XXXXXXX, KC_RBRC, XXXXXXX,                       MO(_FN),  KC_DEL,     XXXXXXX
 ),
 
 [_NAV] = LAYOUT_split_3x5_3(
