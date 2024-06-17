2d00424c7ff3359a8cae6fefaa2e8f1e6930c69c
Travis Mick <root@lo.calho.st>
2024-06-17 19:10:10 +0000

    update keymap for 34 keys

--- keyboards/eggsworks/chicklet/keymaps/default/keymap.c
+++ keyboards/eggsworks/chicklet/keymaps/default/keymap.c
@@ -3,10 +3,11 @@
 
 #include QMK_KEYBOARD_H
 
-enum layer_names {
-    _BASE,
-    _NUM,
-    _NAV
+enum layers {
+  _BASE,
+  _NUM,
+  _NAV,
+  _FN,
 };
 
 #define M1(x) LSFT_T(x)
@@ -15,25 +16,33 @@ enum layer_names {
 #define M4(x) LALT_T(x)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+
 [_BASE] = LAYOUT(
-       KC_Q,       KC_W,         KC_E,        KC_R,             KC_T,                 KC_Y,        KC_U,        KC_I,        KC_O,        KC_P,
-   M1(KC_A),   M2(KC_S),     M3(KC_D),    M4(KC_F),             KC_G,                 KC_H,    M4(KC_J),    M3(KC_K),    M2(KC_L), M1(KC_SCLN),
-   M2(KC_Z),   M3(KC_X),     M4(KC_C),        KC_V,             KC_B,                 KC_N,        KC_M,     KC_COMM,      KC_DOT,     KC_SLSH,
-                KC_LBRC,      KC_RBRC,     KC_BSLS, LT(_NUM, KC_SPC),     LT(_NAV, KC_ENT),     KC_MINS,     KC_QUOT,     KC_BSPC
+       KC_Q,       KC_W,         KC_E,        KC_R,    KC_T,      KC_Y,        KC_U,        KC_I,        KC_O,        KC_P,
+   M1(KC_A),   M2(KC_S),     M3(KC_D),    M4(KC_F),    KC_G,      KC_H,    M4(KC_J),    M3(KC_K),    M2(KC_L), M1(KC_SCLN),
+   M2(KC_Z),   M3(KC_X),     M4(KC_C),        KC_V,    KC_B,      KC_N,        KC_M,     KC_COMM,      KC_DOT,     KC_SLSH,
+   XXXXXXX,    XXXXXXX,       KC_LBRC,     LT(_NUM, KC_SPC),       LT(_NAV, KC_ENT),     KC_BSPC,     XXXXXXX,     XXXXXXX
 ),
 
 [_NUM] = LAYOUT(
-       KC_1,        KC_2,        KC_3,        KC_4,             KC_5,                 KC_6,        KC_7,        KC_8,        KC_9,        KC_0,
-M1(KC_EXLM),   M2(KC_AT), M3(KC_HASH),  M4(KC_DLR),          KC_PERC,              KC_CIRC, M4(KC_AMPR), M3(KC_ASTR), M2(KC_LPRN), M1(KC_RPRN),
-      KC_F1,       KC_F2,       KC_F3,       KC_F4,            KC_F5,                KC_F6,       KC_F7,       KC_F8,       KC_F9,      KC_F10,
-                  KC_F11,      KC_F12,     XXXXXXX,          XXXXXXX,              XXXXXXX,      KC_GRV,      KC_EQL,      KC_DEL
+       KC_1,        KC_2,        KC_3,        KC_4,    KC_5,      KC_6,        KC_7,        KC_8,        KC_9,        KC_0,
+M1(KC_EXLM),   M2(KC_AT), M3(KC_HASH),  M4(KC_DLR), KC_PERC,   KC_CIRC, M4(KC_AMPR), M3(KC_ASTR), M2(KC_LPRN), M1(KC_RPRN),
+      KC_F1,       KC_F2,       KC_F3,       KC_F4,   KC_F5,    KC_GRV,     KC_QUOT,     KC_MINS,      KC_EQL,     KC_BSLS,
+    XXXXXXX,     XXXXXXX,      KC_RBRC,     XXXXXXX,                   MO(_FN),            KC_DEL,    XXXXXXX,     XXXXXXX
 ),
 
 [_NAV] = LAYOUT(
-     KC_ESC,     KC_PGUP,       KC_UP,     KC_PGDN,          KC_VOLU,              XXXXXXX,     XXXXXXX,      KC_INS,     XXXXXXX,     KC_PSCR,
-    XXXXXXX,     KC_LEFT,     KC_DOWN,     KC_RGHT,          KC_VOLD,              XXXXXXX, M4(XXXXXXX), M3(XXXXXXX), M2(XXXXXXX), M1(XXXXXXX),
-    XXXXXXX,     KC_HOME,      KC_ESC,      KC_END,          KC_MUTE,              XXXXXXX,     XXXXXXX,     XXXXXXX,     XXXXXXX,     XXXXXXX,
-                 XXXXXXX,     XXXXXXX,     XXXXXXX,           KC_TAB,              XXXXXXX,     XXXXXXX,     XXXXXXX,      KC_DEL
+     KC_ESC,     KC_PGUP,       KC_UP,     KC_PGDN, KC_VOLU,   XXXXXXX,     XXXXXXX,      KC_INS,     XXXXXXX,     KC_PSCR,
+    XXXXXXX,     KC_LEFT,     KC_DOWN,     KC_RGHT, KC_VOLD,   XXXXXXX, M4(XXXXXXX), M3(XXXXXXX), M2(XXXXXXX), M1(XXXXXXX),
+    XXXXXXX,     KC_HOME,      KC_ESC,      KC_END, KC_MUTE,   XXXXXXX,     XXXXXXX,     XXXXXXX,     XXXXXXX,     XXXXXXX,
+    XXXXXXX,     XXXXXXX,     XXXXXXX,          KC_TAB,               _______,            KC_DEL,     XXXXXXX,     XXXXXXX
+),
+
+[_FN] = LAYOUT(
+      KC_F1,       KC_F2,       KC_F3,       KC_F4,   KC_F5,     KC_F6,       KC_F7,       KC_F8,       KC_F9,      KC_F10,
+     KC_F11,      KC_F12,     XXXXXXX,     XXXXXXX, XXXXXXX,   XXXXXXX,     XXXXXXX,     XXXXXXX,     XXXXXXX,     XXXXXXX,
+    XXXXXXX,     XXXXXXX,     XXXXXXX,     XXXXXXX, XXXXXXX,   XXXXXXX,     XXXXXXX,     XXXXXXX,     XXXXXXX,     XXXXXXX,
+    XXXXXXX,     XXXXXXX,     XXXXXXX,         XXXXXXX,               _______,           XXXXXXX,     XXXXXXX,     XXXXXXX
 ),
 
 };
--- keyboards/eggsworks/chicklet/rules.mk
+++ keyboards/eggsworks/chicklet/rules.mk
@@ -2,3 +2,4 @@ CUSTOM_MATRIX = lite
 VPATH += drivers/gpio
 SRC += pca9555.c matrix.c i2c_master.c
 I2C_DRIVER_REQUIRED = yes
+CONSOLE_ENABLE = yes
